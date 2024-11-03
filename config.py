3bd7c697f3fcb64fedfa4f439c6ebe145ea713d9
Vip-HackerZ <ommore2021107@gmail.com>
2024-11-03 20:01:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7363610742:AAHRdYOwGwg63pc3xnnFh1ZsEDDsOdhTpOM")
-    API_ID = int(os.environ["API_ID", 21179966]
-    API_HASH = os.environ["API_HASH", "d97919fb0a3c725e8bb2a25bbb37d57c"]
-    AUTH_USERS = "7326397503"""
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7514558685:AAFSAm0JYFc0mxhKGdTKjEu4HfJGTJuFpE8")
+    API_ID = int(os.environ["API_ID", "18156248"]
+    API_HASH = os.environ["API_HASH", "db946fb6805b1a698c679626b617e77a"]
+    AUTH_USERS = "2032347579"""
  
